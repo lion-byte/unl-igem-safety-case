7d832a3324bc2453895ab7fbca29fd334b6d9096
Mark Hernandez <mark@lion-byte.com>
2018-07-10 15:09:28 -0500

    Add diagram saving functionality

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,5 @@
+export * from './diagram'
 export * from './lazyload'
+export * from './token'
 
 export const isProduction = () => process.env.NODE_ENV === 'production'
-
-export const setToken = token => window.localStorage.setItem('token', token)
-export const getToken = () => window.localStorage.getItem('token')
-export const removeToken = () => window.localStorage.removeItem('token')
